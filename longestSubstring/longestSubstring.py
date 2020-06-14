3b113892039c02c7ca6c91a12542aec0c6f621da
cvonsteg <cvonstegmann@gmail.com>
2020-06-14 19:31:52 +0100

    added info for longest substrings

--- longestSubstring/longestSubstring.py
+++ longestSubstring/longestSubstring.py
@@ -1,17 +1,22 @@
+"""
+Problem Name: Longest Substring without Repeating Characters
+Difficulty: Medium
+Runtime: 52ms (beats 90.02% of submissions)
+Memory: 14MB (beats 34.71% of submissions)
+"""
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         longest = 0
-        lngth = len(s)
-        for idx, ltr in enumerate(s):
-            new_str = ltr
-            idx += 1
-            while idx < lngth:
-                tmp = s[idx]
-                if tmp in new_str:
-                    break
-                else:
-                    new_str += tmp
-                    idx += 1               
-            if len(new_str) > longest:
-                longest = len(new_str)
+        l_str = ''
+        for ltr in s:
+            if ltr not in l_str:
+                l_str += ltr
+            else:
+                longest = max(longest, len(l_str))
+                ltr_idx = l_str.find(ltr)
+                l_str = l_str[ltr_idx + 1:]
+                l_str += ltr
+        longest = max(longest, len(l_str))
         return longest
